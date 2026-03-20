a093b869e3d9e93896763ff6abea531832ce2f8e
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2026-03-20 19:35:20 +0100

    Setup comments

---
+++ contents/questions.js
@@ -0,0 +1,75 @@
+var questions = {
+  title: "Current Affairs",
+  questionsList: [
+    {
+      question: "Who is the current King of the United Kingdom?",
+      options: [
+        { id: 1, option: "Prince William" },
+        { id: 2, option: "King Charles III" },
+        { id: 3, option: "Prince Harry" },
+        { id: 4, option: "King Edward VII" },
+      ],
+      correctAnswer: 2,
+    },
+    {
+      question: "Which country has the largest population in the world as of 2024?",
+      options: [
+        { id: 1, option: "China" },
+        { id: 2, option: "USA" },
+        { id: 3, option: "India" },
+        { id: 4, option: "Indonesia" },
+      ],
+      correctAnswer: 3,
+    },
+    {
+      question: "What is the capital city of France?",
+      options: [
+        { id: 1, option: "Lyon" },
+        { id: 2, option: "Marseille" },
+        { id: 3, option: "Bordeaux" },
+        { id: 4, option: "Paris" },
+      ],
+      correctAnswer: 4,
+    },
+    {
+      question: "Which organ in the human body is primarily responsible for pumping blood?",
+      options: [
+        { id: 1, option: "Lungs" },
+        { id: 2, option: "Heart" },
+        { id: 3, option: "Kidneys" },
+        { id: 4, option: "Liver" },
+      ],
+      correctAnswer: 2,
+    },
+    {
+      question: "Who is the current Secretary-General of the United Nations?",
+      options: [
+        { id: 1, option: "António Guterres" },
+        { id: 2, option: "Kofi Annan" },
+        { id: 3, option: "Ban Ki-moon" },
+        { id: 4, option: "Donald Trump" },
+      ],
+      correctAnswer: 1,
+    },
+    {
+      question: "Which African country is officially known as the 'Giant of Africa'?",
+      options: [
+        { id: 1, option: "Egypt" },
+        { id: 2, option: "South Africa" },
+        { id: 3, option: "Nigeria" },
+        { id: 4, option: "Ethiopia" },
+      ],
+      correctAnswer: 3,
+    },
+    {
+      question: "Which planet is known as the 'Red Planet'?",
+      options: [
+        { id: 1, option: "Jupiter" },
+        { id: 2, option: "Mars" },
+        { id: 3, option: "Saturn" },
+        { id: 4, option: "Venus" },
+      ],
+      correctAnswer: 2,
+    },
+  ],
+};
